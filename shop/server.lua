98495786fec87f80e160032c3b0ae6f350ee7c7e
Epyi <epyi.contact@gmail.com>
2024-04-11 22:50:47 +0200

    format: Format overall

--- shop/server.lua
+++ shop/server.lua
@@ -1,39 +1,38 @@
-RegisterNetEvent('buyItem', function(itemName)
-    local source = source
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local Playermoney = xPlayer.getMoney()
+RegisterNetEvent("buyItem", function(itemName)
+	local source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local Playermoney = xPlayer.getMoney()
 
-    local itemPrice
-    for _,v in pairs(Config.Nourriture) do
-        if v.item == itemName then
-            itemPrice = v.price
-        end
-    end
-    if not itemPrice then
-        for _,v in pairs(Config.Boisson) do
-            if v.item == itemName then
-                itemPrice = v.price
-            end
-        end
-    end
-    if not itemPrice then
-        return
-    end
+	local itemPrice
+	for _, v in pairs(Config.Nourriture) do
+		if v.item == itemName then
+			itemPrice = v.price
+		end
+	end
+	if not itemPrice then
+		for _, v in pairs(Config.Boisson) do
+			if v.item == itemName then
+				itemPrice = v.price
+			end
+		end
+	end
+	if not itemPrice then
+		return
+	end
 
-    if Playermoney >= itemPrice then
-        if xPlayer ~= nil then
-            xPlayer.removeMoney(itemPrice)
-            xPlayer.addInventoryItem(itemName, 1)
-        end
-    else
-        TriggerClientEvent('ox_lib:notify', source, {
-            
-            title = 'Erreur',
-            description = 'Vous ne possédez pas assez d\'argent pour acheter cet article.',
-            position = 'top',
-            duration = 5000,
-            type = "error",
-        })
-    end
+	if Playermoney >= itemPrice then
+		if xPlayer ~= nil then
+			xPlayer.removeMoney(itemPrice)
+			xPlayer.addInventoryItem(itemName, 1)
+		end
+	else
+		TriggerClientEvent("ox_lib:notify", source, {
+
+			title = "Erreur",
+			description = "Vous ne possédez pas assez d'argent pour acheter cet article.",
+			position = "top",
+			duration = 5000,
+			type = "error",
+		})
+	end
 end)
-
